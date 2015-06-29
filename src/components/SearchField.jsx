7e148930d8eef5edc96494a0c07cbe55686b15b2
Michal Weiser <michal.weiser@gooddata.com>
2015-06-29 09:43:08 +0200

    CODE: Searchfield extension

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -2,14 +2,23 @@ import { Component } from 'react';
 
 export default class SearchField extends Component {
     onChange(ev) {
-        this.props.onChange(ev.target.value);
+        this.props.onChange && this.props.onChange(ev.target.value);
+    }
+
+    onKeyPress(ev) {
+        if (ev.key === "Escape") {
+            this.props.onClear && this.props.onClear();
+        } else if (ev.key === "Enter") {
+            this.props.onSubmit && this.props.onSubmit(ev.target.value);
+        }
     }
 
     render() {
         return (
             <input
-                className="search-field"
+                className="input-text searchfield"
                 onChange={this.onChange.bind(this)}
+                onKeyDown={this.onKeyPress.bind(this)}
                 value={this.props.value}
                 />
         );
@@ -17,3 +26,10 @@ export default class SearchField extends Component {
 }
 
 SearchField.displayName = 'SearchField';
+
+SearchField.propTypes = {
+    value: React.PropTypes.string.isRequired,
+    onChange: React.PropTypes.function,
+    onClear: React.PropTypes.function,
+    onSubmit: React.PropTypes.function
+};
